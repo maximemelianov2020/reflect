9474e27ceeb4060740ffc7161b56d24541eb5264
maximemelianov2020 <maximemelianov2020@gmail.com>
2025-12-24 07:56:14 +0300

    Update SODoer.java

--- src/main/java/lab/implementations/SODoer.java
+++ src/main/java/lab/implementations/SODoer.java
@@ -1,4 +1,13 @@
 package lab.implementations;
 
-public class SODoer {
+import lab.interfaces.SomeOtherInterface;
+
+/**
+ * Реализация SomeOtherInterface, выводящая "C".
+ */
+public class SODoer implements SomeOtherInterface {
+    @Override
+    public void doSomeOther() {
+        System.out.print("C");
+    }
 }
