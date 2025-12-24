a012864436704cc9b10e012c0f25bce5c4a534e7
maximemelianov2020 <maximemelianov2020@gmail.com>
2025-12-24 07:55:25 +0300

    Update SomeImpl.java

--- src/main/java/lab/implementations/SomeImpl.java
+++ src/main/java/lab/implementations/SomeImpl.java
@@ -1,4 +1,13 @@
 package lab.implementations;
 
-public class SomeImpl {
+import lab.interfaces.SomeInterface;
+
+/**
+ * Реализация SomeInterface, выводящая "A".
+ */
+public class SomeImpl implements SomeInterface {
+    @Override
+    public void doSomething() {
+        System.out.print("A");
+    }
 }
