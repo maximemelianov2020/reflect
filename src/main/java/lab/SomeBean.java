686322e4fbed727023dc4737b5ad01b7bc3f029b
maximemelianov2020 <maximemelianov2020@gmail.com>
2025-12-24 08:01:47 +0300

    Update SomeBean.java

--- src/main/java/lab/SomeBean.java
+++ src/main/java/lab/SomeBean.java
@@ -1,4 +1,25 @@
 package lab;
 
+import lab.annotation.AutoInjectable;
+import lab.interfaces.SomeInterface;
+import lab.interfaces.SomeOtherInterface;
+
+/**
+ * Пример класса с полями, помеченными аннотацией @AutoInjectable.
+ * Эти поля должны быть автоматически инициализированы Injector'ом.
+ */
 public class SomeBean {
+    @AutoInjectable
+    private SomeInterface field1;
+
+    @AutoInjectable
+    private SomeOtherInterface field2;
+
+    /**
+     * Метод, использующий внедренные зависимости.
+     */
+    public void foo() {
+        field1.doSomething();
+        field2.doSomeOther();
+    }
 }
