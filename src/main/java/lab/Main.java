7bd80a782f7fcdeed2e8002d16aa48d9a57800ac
maximemelianov2020 <maximemelianov2020@gmail.com>
2025-12-24 08:00:07 +0300

    Update Main.java

--- src/main/java/lab/Main.java
+++ src/main/java/lab/Main.java
@@ -1,4 +1,19 @@
 package lab;
 
+import lab.SomeBean;
+import lab.injector.Injector;
+
+/**
+ * Демонстрационный класс для запуска примера.
+ */
 public class Main {
+    public static void main(String[] args) {
+        Injector injector = new Injector();
+        SomeBean bean = injector.inject(new SomeBean());
+
+        System.out.println("Результат выполнения:");
+        bean.foo(); // Должно вывести AC или BC
+
+        System.out.println("\nЗависимости успешно внедрены!");
+    }
 }
